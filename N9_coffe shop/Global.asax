4b6f6a373636a8b06d1a845df1e529c25a2ae9ab
mossbel <65538526+mossbel@users.noreply.github.com>
2020-05-18 21:03:31 +0700

    Update

---
+++ N9_coffe shop/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="N9_coffe_shop.MvcApplication" Language="C#" %>
---
+++ N9_coffe shop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(N9_coffe_shop.Startup))]
+namespace N9_coffe_shop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
